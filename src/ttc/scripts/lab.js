848ce50f5764ecf2dd0e01260a8336a2f7649482
Kamarov <153866697+kamarov-therussiantank@users.noreply.github.com>
2026-05-12 01:41:45 +0200

    fix: update image paths and add "flow-root" display for shop items wrapper

--- src/ttc/scripts/lab.js
+++ src/ttc/scripts/lab.js
@@ -42,7 +42,7 @@ function loadShopElements() {
             <tbody>
                 <tr>
                     <td>
-                        <img src="https://raw.githubusercontent.com/kamarov-therussiantank/TTCV2/refs/heads/main/src/assets/lab/testTube.png" alt="Test Tube">
+                        <img src="https://raw.githubusercontent.com/kamarov-therussiantank/TTC/refs/heads/main/src/assets/images/lab/testTube.png" alt="Test Tube">
                     </td>
                     <td>
                         <br>
@@ -89,15 +89,15 @@ function loadShopElements() {
             }
         </style>
         <div style="margin-top: 30px; clear: both;">
-            <img style="float: left; margin-right: 15px; margin-bottom: 30px; margin-left: 60px; width: 200px;" src="https://raw.githubusercontent.com/kamarov-therussiantank/TTCV2/refs/heads/main/src/assets/lab/TTTradingCardsSeriesISpread.png">
+            <img style="float: left; margin-right: 15px; margin-bottom: 30px; margin-left: 60px; width: 200px;" src="https://raw.githubusercontent.com/kamarov-therussiantank/TTC/refs/heads/main/src/assets/images/lab/TTTradingCardsSeriesISpread.png">
             <span style="font-size: 18px; font-family: 'TankTrouble';">TankTrouble Trading Cards</span><br>
             The classic trump card game with notorious tanks. Print them for your own deck of portable destruction. Special thanks goes for TLR Team for development.<br><br>
-            <a href="https://drive.google.com/file/d/1RsTjGg2ZCMWzKTXmWt02PldxvJvfplM-/view" target="_blank">Right click here to download series I</a><br>
-            <a href="https://drive.google.com/file/d/1jbOKhs6BXFougHo5iXT3KHu7-lr-PtJy/view" target="_blank">Right click here to download series II</a><br>
-            <a href="https://drive.google.com/file/d/1pue7F1QCeEeL6OlhC2hf8M-FssDN9C3p/view" target="_blank">Right click here to download series III</a><br>
-            <a href="https://drive.google.com/file/d/1yKroGO_YiCwaMaWtcyfvH1MKVjLbcH8_/view" target="_blank">Right click here to download series IV</a><br>
-            <a href="https://drive.google.com/file/d/1oOnM-dj-v5kHrhwUpjtG7DR3fzFBLf9I/view" target="_blank">Right click here to download series V</a><br>
-            <a href="https://drive.google.com/file/d/1TwztT7ZRR8dlkSC4VyI94zjvWcuUMGNy/view" target="_blank">Right click here to download series VI</a><br>
+            <a href="https://drive.google.com/file/d/1RsTjGg2ZCMWzKTXmWt02PldxvJvfplM-/view" target="_blank">Checkout series I</a><br>
+            <a href="https://drive.google.com/file/d/1jbOKhs6BXFougHo5iXT3KHu7-lr-PtJy/view" target="_blank">Checkout series II</a><br>
+            <a href="https://drive.google.com/file/d/1pue7F1QCeEeL6OlhC2hf8M-FssDN9C3p/view" target="_blank">Checkout series III</a><br>
+            <a href="https://drive.google.com/file/d/1yKroGO_YiCwaMaWtcyfvH1MKVjLbcH8_/view" target="_blank">Checkout series IV</a><br>
+            <a href="https://drive.google.com/file/d/1oOnM-dj-v5kHrhwUpjtG7DR3fzFBLf9I/view" target="_blank">Checkout series V</a><br>
+            <a href="https://drive.google.com/file/d/1TwztT7ZRR8dlkSC4VyI94zjvWcuUMGNy/view" target="_blank">Checkout series VI</a><br>
         </div>
     `, 'mainContent', 'shopItemsWrapper');
     insertElement('div', {
@@ -109,12 +109,12 @@ function loadShopElements() {
             }
         </style>
         <div style="margin-top: 30px; clear: both;">
-	<img style="float: left; margin-right: 15px; margin-bottom: 30px; margin-left: 60px; width: 200px;" src="https://raw.githubusercontent.com/kamarov-therussiantank/TTCV2/refs/heads/main/src/assets/lab/ttessentials.png">
+	<img style="float: left; margin-right: 15px; margin-bottom: 30px; margin-left: 60px; width: 200px;" src="https://raw.githubusercontent.com/kamarov-therussiantank/TTC/refs/heads/main/src/assets/images/lab/ttessentials.png">
 	<span style="font-size: 18px; font-family: 'TankTrouble';">Kickstarter Latecomer's Shop & Dimitri's Emporium Extras</span><br>
-	If you missed the initial Kickstarter campaign, you can still get your hands on some exclusive items! The Latecomer's Shop offers a variety of unique products.<br>
-    But what about those cool things beside the Backer Box? You may want to click here:<br><br>
+	If you missed the initial Kickstarter campaign, you can still get your hands on some exclusive items! The Latecomer's Shop offers a variety of unique products.<br><br>
+    Looking for those cool looking hoodies or t-shirts? You may want to click here:<br><br>
 	<a href="https://tanktrouble.myspreadshop.com/" target="_blank">https://tanktrouble.myspreadshop.com/</a><br><br>
-	All purchases contribute to the ongoing development of TankTrouble. Thanks for your support!</div>
+	All purchases contribute to the ongoing development of TankTrouble. Thanks for your support!</div><br><br><br><br><br><br>
     `, 'mainContent', 'shopItemsWrapper');
 }
 function waitForShopPage() {
